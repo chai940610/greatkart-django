66c84fd52784e4f9f128f7d7bd0e486ddbe1bac1
John <chai940610@hotmail.com>
2022-11-12 17:54:17 +0800

    modified JS and image

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -127,11 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=BASE_DIR/'static'
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS=[      #for my understanding, this one is used when you use collectstatic, so when you use collect static, the new static file will occur at the outside greatkart, once you collectstati, there got one admin thing inside the folder
-    'greatkart/static',
+   'greatkart\static'
 ]
-# media files configuration
+#media files configuration
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR/'media'
 
